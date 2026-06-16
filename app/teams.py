4c6cc2d22fe659356169ff364147f751bfb40e06
SaloEater <danil.lazarev@gmail.com>
2026-06-16 09:03:28 -0500

    fix: miscellaneous spot

--- app/teams.py
+++ app/teams.py
@@ -5,7 +5,7 @@ TEAMS: list[str] = sorted([
     "Denver Broncos", "Detroit Lions", "Green Bay Packers",
     "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars",
     "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers",
-    "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings",
+    "Los Angeles Rams", "Miami Dolphins", "Miscellaneous", "Minnesota Vikings",
     "New England Patriots", "New Orleans Saints", "New York Giants",
     "New York Jets", "Philadelphia Eagles", "Pittsburgh Steelers",
     "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers",
